8762338a09b7c2e10174e7a7dabcd7b85fc8b3f6
Tatiana <tanik.sam@yandex.ru>
2022-05-25 12:22:01 +0300

    fix: 04-copy-directory, actualization, (Wed, May 25, 2022 00:21:00 PM)

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -2,30 +2,32 @@ const fs = require('fs');
 const path = require('path');
 const fsPromises = fs.promises;
 
-fsPromises.mkdir(path.join(__dirname, 'files-copy')).then(function () {
-    console.log('Директория успешно создана');
-}).catch(function () {
-    console.log('Директория не создана');
-});
-
-fs.readdir(path.join(__dirname, 'files'), { withFileTypes: true }, (err, files) => {
-    if (err)
-        console.log("Не могу прочитать директорию files", err);
-    else {
-        console.log("\nСкопированные файлы:")
-        files.forEach(file => {
-            if (file.isFile()) {
-                fs.copyFile(path.join(__dirname, 'files', file.name), path.join(__dirname, 'files-copy', file.name),
-                    fs.constants.COPYFILE_EXCL, (err) => {
-                        if (err) {
-                            console.log(`\nА такой файл ${file.name}  директории files-copy уже есть!:`);
+(async function () {
+    try {
+        await fsPromises.rm(path.join(__dirname, 'files-copy'), { force: true, recursive: true })
+        await fsPromises.mkdir(path.join(__dirname, 'files-copy')).then(function () {
+            console.log('Директория успешно создана');
+        }).then(
+            fs.readdir(path.join(__dirname, 'files'), { withFileTypes: true }, (err, files) => {
+                if (err)
+                    console.log("Не могу прочитать директорию files", err);
+                else {
+                    console.log("\nСкопированные файлы:")
+                    files.forEach(async (file) => {
+                        if (file.isFile()) {
+                            fs.copyFile(path.join(__dirname, 'files', file.name), path.join(__dirname, 'files-copy', file.name),
+                                fs.constants.COPYFILE_EXCL, (err) => {
+                                    if (err) {
+                                        console.log(`\nА такой файл ${file.name}  директории files-copy уже есть!:`);
+                                    }
+                                    else {
+                                        console.log(file.name)
+                                    }
+                                });
                         }
-                        else {
-                            console.log(file.name)
-                        }
-                    });
-            }
-        })
-    }
-})
+                    })
+                }
+            }))
 
+    } catch (err) { console.log(error) }
+})()
